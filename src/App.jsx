7870b14eb2a3c6cc651c613d99cbe48fc0e77505
Hoplite2A <stgdave.mailloux#gmail.com>
2023-11-04 21:03:51 -0400

    completed 'Adding CSS' and did not finish 'Wrapping Up' as I continuously received errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 import { puppyList } from "./data";
-// import "./App.css";r
+// import "./index.css";
+import "./App.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
   return (
-    <>
-      <div>
-        {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
-        })}
-      </div>
-    </>
+    <div className="thaBiggerDiv">
+      {featPupId && (
+        <div className="thaBigDiv">
+          <h2 className="bigPuppa">{featuredPup.name}</h2>
+          <h5 className="thaDeets">Age: {featuredPup.age}</h5>
+          <h5 className="thaDeets">Email: {featuredPup.email}</h5>
+          <br></br>
+        </div>
+      )}
+      {puppies.map((puppy) => {
+        return (
+          <h4
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </h4>
+        );
+      })}
+    </div>
   );
 }
 
